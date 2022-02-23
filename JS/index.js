85537ed0f19cf0f621ce05a9cf1c0b21d37be8e1
unknown <manishtomar998@gmail.com>
2022-02-23 16:12:28 +0530

    login JS completed

---
+++ JS/index.js
@@ -0,0 +1,23 @@
+let login = (event) => {
+    event.preventDefault();
+    let loginEmail = document.getElementById("email").value;
+    let loginPassword = document.getElementById("password").value;
+    fetch("http://localhost:3000/users").then(response => {
+        response.text().then(res => {
+            findUser(JSON.parse(res), loginEmail, loginPassword)
+        })
+    })
+}
+let findUser = (res, loginEmail, loginPassword) => {
+    let user = res.filter(data => data.email === loginEmail);
+    if (user.length == 0) {
+        alert("user not found");
+    } else {
+        if (user[0].password !== loginPassword) {
+            alert("wrong password");
+        } else {
+            location.replace("./Pages/home.html");
+        }
+    }
+
+}
